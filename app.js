2661d8e50c826ae06c5d2e73d32f2d7a47bd5840
amosvictor97 <amosatsuvia@gmail.com>
2022-05-11 11:11:00 +0000

    mongoose installation and configuration

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var shipmentsRouter = require('./routes/shipments');
+const indexRouter = require('./routes/index');
+const shipmentsRouter = require('./routes/shipments');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//connect to mongodb
+const dbURI= "mongodb+srv://deliveryos:deliveryos2022@realmcluster.tpgga.mongodb.net/deliveryos-shipment-api?retryWrites=true&w=majority"
+mongoose.connect(dbURI)
+    .then(result => console.log('connected to the db'))
+    .catch(err => console.log(err))
+
 
 app.use('/', indexRouter);
 app.use('/shipments', shipmentsRouter);
